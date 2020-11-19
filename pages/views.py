f6beb44c69cd9722a3270f8833a4d45bc56367e2
Precious Chika <43546663+pre22@users.noreply.github.com>
2020-11-19 17:34:38 +0100

    chore: created views and templates
    wrote views for the extra templates.
    Created more templates

--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,12 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'pages/index.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+class GalleryPageView(TemplateView):
+    template_name = 'pages/gallery.html'
+
+class ContactPageView(TemplateView):
+    template_name = 'pages/contact.html'
